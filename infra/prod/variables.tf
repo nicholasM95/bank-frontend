2c3ee1385fb3f2a679ff51361061ae6dccafab60
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-09-19 00:57:10 +0200

    chore: modify csp

--- infra/prod/variables.tf
+++ infra/prod/variables.tf
@@ -1,7 +1,7 @@
 variable "content_security_policy" {
   type        = string
   description = "Content Security Policy header"
-  default     = "frame-ancestors 'none' bank.nicholasmeyers.be; default-src 'none'; img-src 'self'; script-src 'self' 'unsafe-inline'; script-src-elem 'self'; style-src 'self' 'unsafe-inline'; object-src 'none'; font-src 'self' fonts.gstatic.com; connect-src 'self' keycloak.meyersnicholas.be bank-api.app-meyers.be; base-uri 'self'; manifest-src 'self'; form-action 'self'; frame-src 'self' keycloak.meyersnicholas.be; worker-src 'self'"
+  default     = "frame-ancestors bank.nicholasmeyers.be; default-src 'none'; img-src 'self'; script-src 'self' 'unsafe-inline'; script-src-elem 'self'; style-src 'self' 'unsafe-inline'; object-src 'none'; font-src 'self' fonts.gstatic.com; connect-src 'self' keycloak.meyersnicholas.be bank-api.app-meyers.be; base-uri 'self'; manifest-src 'self'; form-action 'self'; frame-src 'self' keycloak.meyersnicholas.be; worker-src 'self'"
 }
 
 variable "permission_policy" {
